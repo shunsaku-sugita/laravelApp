ff2d6d6522e80a70ca33865449fd578887632da1
shunsaku sugita <saku.dot@gmail.com>
2020-03-13 01:14:05 +0900

    First Commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('hello.index');
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = [
+            'msg' => 'こんにちは、'. $msg . 'さん！',
+        ];
+        return view('hello.index', $data);
+    }
+}
---
+++ app/Providers/HelloServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class HelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer(
+          'hello.index', 'App\Http\Composers\HelloComposer');
+    }
+}
