e105861730ae4ad662c1fe0bcb56fbcd0b1ed25d
shunsaku sugita <saku.dot@gmail.com>
2020-03-18 00:22:55 +0900

    バリデーションなど

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,20 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index');
+        return view('hello.index', ['msg' => 'フォームを入力：']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $msg = $request->msg;
-        $data = [
-            'msg' => 'こんにちは、'. $msg . 'さん！',
-        ];
-        return view('hello.index', $data);
+        return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
