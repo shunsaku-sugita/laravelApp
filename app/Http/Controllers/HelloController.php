5d4e0c3f2819e8600d8151ac8195eb647fda04bd
shunsaku sugita <saku.dot@gmail.com>
2020-03-21 16:40:16 +0900

    バリデータの基本

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -12,8 +13,28 @@ class HelloController extends Controller
         return view('hello.index', ['msg' => 'フォームを入力：']);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric | between:0, 150',
+        ]);
+
+        // 失敗した場合true
+//        if ($validator->fails()) {
+//            return redirect('/hello')
+//                        ->withErrors($validator)
+//                        ->withInput();
+//        }
+
+        // 成功した場合true
+        if (!$validator->passes()) {
+            return redirect('/hello')
+                        ->withErrors($validator) // エラーメッセージをリダイレクト先まで引き継ぐ
+                        ->withInput(); // 入力値をリダイレクト先まで引き継ぐ
+        }
+
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
