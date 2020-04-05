e75f57504bcdbfa5d37517894631843ad70ae448
shunsaku sugita <saku.dot@gmail.com>
2020-04-05 22:13:42 +0900

    DB周りの設定の続き

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,19 +6,15 @@ use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg' => $msg]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
-
     public function post(Request $request)
     {
         $validate_rule = [
